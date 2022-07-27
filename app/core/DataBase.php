6c81811f5ff9bf05062ffeec328cfeb5be3058ae
lucas cardoso <lucas.cardo123@hotmail.com>
2022-07-27 16:13:59 -0400

    update config FETCH_OBJ for FETCH_ASSOC

--- app/core/DataBase.php
+++ app/core/DataBase.php
@@ -33,7 +33,7 @@ class DataBase
             $this->senha,
         [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_CASE => PDO::CASE_NATURAL
         ]
             );
