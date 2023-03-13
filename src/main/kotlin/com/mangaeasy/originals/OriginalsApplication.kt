110a77ead7d9466bfd9c99b4af2396bb77ffb3f0
JhonnyBaltros <jhonnybaixo@gmail.com>
2023-03-12 22:01:21 -0300

    Fixing error of container already exists and creating config class.

--- src/main/kotlin/com/mangaeasy/originals/OriginalsApplication.kt
+++ src/main/kotlin/com/mangaeasy/originals/OriginalsApplication.kt
@@ -1,9 +1,12 @@
 package com.mangaeasy.originals
 
+import com.mangaeasy.originals.configs.Configs
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Import
 
 @SpringBootApplication
+@Import(Configs::class)
 class OriginalsApplication
 
 fun main(args: Array<String>) {
---
+++ src/main/kotlin/com/mangaeasy/originals/configs/Configs.kt
@@ -0,0 +1,8 @@
+package com.mangaeasy.originals.configs
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.PropertySource
+
+@Configuration
+@PropertySource("classpath:application.properties")
+class Configs
