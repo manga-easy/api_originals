f449016b3bae0f88b867bfee7bd4b24470ace222
JhonnyBaltros <jhonnybaixo@gmail.com>
2023-03-20 00:28:50 -0300

    Creating Author entity.

--- src/main/kotlin/com/mangaeasy/originals/domain/Author.kt
+++ src/main/kotlin/com/mangaeasy/originals/domain/Author.kt
@@ -1,16 +1,15 @@
 package com.mangaeasy.originals.domain
 
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
+import jakarta.persistence.*
 import java.io.Serializable
 
+@Table(name = "author")
 @Entity
-data class Author(
+class Author(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Int,
+    @Column(name = "id", nullable = false)
+    var id: Long? = null,
     var name: String? = null,
     var createdAt: String? = null,
     var updatedAt: String? = null
