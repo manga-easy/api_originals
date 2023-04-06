581afe065ab1041171e86d95e76c8cbfbc2cf513
Jhonny Baltros <jhonnybaixo@gmail.com>
2023-04-05 22:56:47 -0300

    Creating initial structure for Work(or comic) entity.

--- src/main/kotlin/com/mangaeasy/originals/domain/Work.kt
+++ src/main/kotlin/com/mangaeasy/originals/domain/Work.kt
@@ -5,33 +5,39 @@ import org.hibernate.Hibernate
 import java.io.Serializable
 import java.util.Date
 
-@Table(name = "author")
+@Table(name = "works")
 @Entity
-data class Author(
+data class Work(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     val id: Long? = null,
-    @Column(name = "name", nullable = false)
-    val name: String? = null,
-    @Column(name = "works", nullable = false)
-    @ManyToOne
-    var works: List<Work>,
+
+    @Column(name = "title", nullable = false)
+    val title: String? = null,
+
+    @Column(name = "authorId", nullable = false)
+    val authorId: Long? = null,
+
+    var sinopse: String? = "",
+    var status: Enum<*>,
+    var pubStatus: Enum<*>,
+    val createdAt: Date = Date(),
     var updatedAt: Date? = null,
-    var createdAt: Date = Date()
 ) : Serializable {
+
+    @Override
+    override fun toString(): String {
+        return this::class.simpleName + "(id = $id , title = $title )"
+    }
+
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other == null || Hibernate.getClass(this) != Hibernate.getClass(other)) return false
-        other as Author
+        other as Work
 
         return id != null && id == other.id
     }
 
     override fun hashCode(): Int = javaClass.hashCode()
-
-    @Override
-    override fun toString(): String {
-        return this::class.simpleName + "(id = $id , name = $name )"
-    }
 }
